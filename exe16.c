e799b18099a9a5e4d276f6be26ecd248fa8e09d5
LorenzoWF <lorenzo.wendt@outlook.com>
2015-11-19 13:09:43 +0000

    Exercicio 16 v02

--- exe16.c
+++ exe16.c
@@ -13,17 +13,25 @@ int main (){
         __fpurge(stdin); // ESTAVA UTILIZANDO UMA MAQUINA VIRTUAL LINUX PARA COMPILAR OS ALGORITMOS, ESSA FUNCAO NO WINDOWS E A FFLUSH, POR FAVOR SUBSTITUA ELA PARA REALIZAR OS TESTES
         fgets(linha, 51, stdin);
         f = 0;
-        for (i=0; linha[i] != '\n'; i++){
+        for (i=0; linha[i] != '\n' && f < 2; i++){
             if (linha[i] == 32){
                 f = 0;
             } else{
                 if (f == 0){
-                    printf("%c", linha[i]);
-                    f = 1;   
+                    if (linha[i] < 97 || linha[i] > 122){ // O a NA TABELA ASCII E O 97 E O z E O 122
+                        f = 2;
+                    } else{
+                        printf("%c", linha[i]);
+                        f = 1;   
+                    }
                 }
             }
         }
-        printf("\n");
+        if (f == 2){
+            printf("\nATENCAO, FOI DIGITADO UM CARACTER INVALIDO NA LINHA ACIMA, POR ISSO A MESNGAEM NAO PODE SER CODIFICADA POR COMPLETA"); 
+        } else {
+            printf("\n");   
+        }
     }
     
     return 0;
