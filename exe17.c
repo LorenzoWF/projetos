5a2871f297bdefaca1883862e1ea753c7e3fff8d
LorenzoWF <lorenzo.wendt@outlook.com>
2015-11-22 04:19:22 +0000

    exe17.c com verificacoes de entrada prontas

--- exe17.c
+++ exe17.c
@@ -3,32 +3,44 @@
 
 int main (){
     
-    int n, i, h, soma, mul, divisao, aux, f, total_1, total_2; 
+    int n, i, soma, mul, divisao, aux, f, total_1, total_2; 
     char nome[10];
     
     scanf("%d", &n);
-    if (n > 0){
+    if (n > 0 && n < 100000){
         do {
             
             int vSoma[n];
             char vNome[n][12];
-            h = 0;
-            
-            for (aux=0; aux<n; aux++){
+            f = 0;
+            for (aux=0; aux<n && f == 0; aux++){
                 soma = 0;
                 __fpurge(stdin);
                 fgets(nome, 12, stdin);
-                for (i=0; nome[i] != '\n'; i++){
-                    soma = soma + nome[i];
-                    vNome[h][i] = nome[i];
+                
+                if (nome[0] < 65 || nome[0] > 90){
+                    f = 2;
+                } 
+                
+                if (aux > 0 && f == 0){
+                    f = 2;
+                    for (i=0; nome[i] != '\n' && f == 2; i++){
+                        if (nome[i] > vNome[aux - 1][i]){
+                            f = 0;   
+                        }
+                    }
                 }
+                
+                for (i=0; nome[i] != '\n' && f == 0; i++){
+                    soma = soma + nome[i];
+                    vNome[aux][i] = nome[i];
+                }
+                
                 vSoma[aux] = soma;
-                vNome[h][i] = '\n';
-                h++;
+                vNome[aux][i] = '\n';
             }
             
             divisao = 0;
-            f = 0;
             
             for (divisao=divisao; divisao < n && f == 0; divisao++){
                 
@@ -62,12 +74,14 @@ int main (){
             
             if (f == 0){
                 printf("Impossibilidade de empate.");
+            } else if (f == 2){
+                printf("Entrada Invalida, digite o numero de estudantes novamente!");
             }
             
             printf("\n");
             
             scanf(" %d", &n);
-        } while (n > 0);   
+        } while (n > 0 && n < 100000);   
     }
     
     return 0;
